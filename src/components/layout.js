5ffa1571f9673fc0ba4b12c5a643a3184e315f99
Cru Scanlan <cruzercru@hotmail.com>
2018-11-30 14:14:52 +1000

    Started Adding Material Kit React

---
+++ src/components/layout.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import { StaticQuery, graphql } from 'gatsby'
+
+import Header from './Header/Header'
+import HeaderLinks from './Header/HeaderLinks'
+import withRoot from "../withRoot";
+
+
+const Layout = ({ children }) => (
+    <StaticQuery
+        query={graphql`
+            query SiteTitleQuery {
+                site {
+                    siteMetadata {
+                        title
+                    }
+                }
+            }
+    `}
+        render={data => (
+            <>
+                <Helmet
+                    title={data.site.siteMetadata.title}
+                    meta={[
+                        { name: 'description', content: 'Sample' },
+                        { name: 'keywords', content: 'sample, something' },
+                    ]}
+                >
+                    <html lang="en" />
+                </Helmet>
+                <Header
+                    color="transparent"
+                    brand="Material Kit React"
+                    fixed
+                    rightLinks={<HeaderLinks />}
+                    changeColorOnScroll={{
+                        height: 100,
+                        color: "white"
+                    }}/>
+                <div
+                    style={{
+                        margin: '0 auto',
+                        maxWidth: 960,
+                        padding: '0px 1.0875rem 1.45rem',
+                        paddingTop: 0,
+                    }}
+                >
+                    {children}
+                </div>
+            </>
+        )}
+    />
+);
+
+Layout.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default withRoot(Layout);
