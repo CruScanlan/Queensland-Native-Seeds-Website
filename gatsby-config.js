20130ddd47a1321630332ab137f51bef453e64e6
Cru Scanlan <cruzercru@hotmail.com>
2018-12-02 11:37:45 +1000

    Hopefully fix gatsby + material kit bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
     siteMetadata: {
-      title: 'Queensland Native Seeds',
+        title: 'Queensland Native Seeds',
     },
     plugins: [
+        'gatsby-plugin-resolve-src',
         {
             resolve: "gatsby-source-prismic",
             options: {
@@ -59,7 +60,7 @@ module.exports = {
             resolve: `gatsby-source-filesystem`,
             options: {
                 name: `images`,
-                path: `${__dirname}/src/images`,
+                path: `${__dirname}/src/assets/img`,
             },
         },
         'gatsby-transformer-sharp',
@@ -73,7 +74,7 @@ module.exports = {
                 background_color: '#663399',
                 theme_color: '#663399',
                 display: 'minimal-ui',
-                icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+                icon: 'src/assets/img/gatsby-icon.png', // This path is relative to the root of the site.
             },
         }
     ]
