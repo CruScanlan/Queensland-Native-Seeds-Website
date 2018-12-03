497a2a2b6bc645d2d566ac61dfe0fe93f4523c3d
Cru Scanlan <cruzercru@hotmail.com>
2018-12-03 17:38:53 +1000

    Add Netlify plugin

--- src/html.js
+++ src/html.js
@@ -13,6 +13,7 @@ export default class HTML extends React.Component {
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
           <link href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" rel="stylesheet" />
+            <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes}>
@@ -28,8 +29,6 @@ export default class HTML extends React.Component {
     )
   }
 }
-
-//<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
 
 HTML.propTypes = {
   htmlAttributes: PropTypes.object,
