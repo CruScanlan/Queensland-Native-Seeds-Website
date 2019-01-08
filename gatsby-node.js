0023826244b3feca7a6db6cbe002c5310dbe64f6
Cru Scanlan <cruzercru@hotmail.com>
2019-01-08 15:33:19 +1000

    Netlify test

--- gatsby-node.js
+++ gatsby-node.js
@@ -18,7 +18,7 @@ async function calculateDirs(store) {
   ]
 
   console.log(program.directory);
-  fs.readdir('/opt/build/cache', function(err, items) {
+  fs.readdir('/opt/build/cache/gatsby', function(err, items) {
     console.log(items);
 
     for (var i=0; i<items.length; i++) {
