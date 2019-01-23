617f2eedae0f9f6c2b2c8600e494cae721af1f10
Cru Scanlan <cruzercru@hotmail.com>
2019-01-23 13:04:53 +1000

    Test plant profiles index

--- src/assets/views/plantProfileIndexStyle.jsx
+++ src/assets/views/plantProfileIndexStyle.jsx
@@ -48,6 +48,67 @@ const plantProfileStyle = {
         width: "100%",
         height: "100%",
         zIndex: "-1"
+    },
+    tableArrowIcon: {
+        float: 'right', 
+        color: '#7DA831', 
+        fontSize: 32,
+        verticalAlign: 'middle'
+    },
+    table: {
+        display: 'table',
+        width: '100%',
+        overflow: 'auto',
+        borderSpacing: 0,
+        borderCollapse: 'collapse'
+    },
+    tableHeader: {
+        display: 'table-header-group'
+    },
+    tableHeaderRow: {
+        height: 'auto',
+        display: 'table-row',
+        verticleAlign: 'middle'
+    },
+    tableHeaderColumn: {
+        padding: '0 10px 0 10px',
+        textAlign: 'left',
+        fontSize: '1.125rem',
+        fontWeight: 500,
+        borderTopWidth: '0 !important',
+        borderBottomWidth: '1px',
+        '&:hover': {
+            backgroundColor: '#f9f9f9',
+            cursor: 'pointer'
+        }
+    },
+    tableBody: {
+        display: 'table-row-group',
+        borderTop: '2px solid #bbb'
+    },
+    tableRow: {
+        height: 'auto',
+        display: 'table-row',
+        verticalAlign: 'middle',
+        '&:hover': {
+            backgroundColor: '#f9f9f9',
+            cursor: 'pointer'
+        }
+    },
+    tableCell: {
+        textAlign: 'left',
+        padding: '12px 10px !important',
+        position: 'relative',
+        borderRight: '1px solid #ddd',
+        borderTop: '1px solid #ddd',
+        verticalAlign: 'middle'
+    },
+    tableCellLast: {
+        textAlign: 'left',
+        padding: '12px 10px !important',
+        position: 'relative',
+        borderTop: '1px solid #ddd',
+        verticalAlign: 'middle'
     }
 };
 
--- src/pages/plant-profiles.jsx
+++ src/pages/plant-profiles.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 // nodejs library that concatenates classes
 import classNames from "classnames";
 import {withStyles} from '@material-ui/core/styles';
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 import Layout from 'components/Layout.jsx';
 import withRoot from '../withRoot';
 
 import ParallaxHeader from "components/Parallax/ParallaxHeader.jsx";
 import GridContainer from "components/Grid/GridContainer.jsx";
 import GridItem from "components/Grid/GridItem.jsx";
+import Badge from "components/Badge/Badge.jsx";
+import CustomInput from "components/CustomInput/CustomInput.jsx";
+import IconArrowDropDown from "@material-ui/icons/ArrowDropDown";
 
 import { cardTitle } from "assets/material-kit-react.jsx";
 import landingPageStyle from "assets/views/landingPage.jsx";
@@ -27,8 +30,33 @@ class PlantProfiles extends React.Component {
 
     }
 
+    createBadges(list, color) {
+        if(!list) return <div></div>
+        return list.map(item => (
+            <Badge color={color} key={item.name}>
+                {item.name}
+            </Badge>
+        ))
+    }
+
     render() {
         const {classes, data} = this.props;
+
+        const plantProfileRows = data.allContentfulPlantProfile.edges.map(plantProfile => {
+            return (
+                <Link to={`/plant-profiles/${plantProfile.node.slug}`} style={{textDecoration: "inherit", color: 'inherit'}} className={classes.tableRow}>
+                    <td className={classes.tableCell}>
+                        {plantProfile.node.scientificName}
+                    </td>
+                    <td className={classes.tableCell}>
+                        {plantProfile.node.commonName ? plantProfile.node.commonName.join(', ') : ''}
+                    </td>
+                    <td className={classes.tableCellLast}>
+                        {this.createBadges(plantProfile.node.categories, 'green')}
+                    </td>
+                </Link>
+            )
+        })
 
         return (
             <Layout>
@@ -40,22 +68,29 @@ class PlantProfiles extends React.Component {
                                 <GridItem xs={12} sm={12} md={12}>
                                     <h2 className={classes.pageTitle}>Plant Profiles</h2>
                                 </GridItem>
-                                <GridItem xs={12} sm={12} md={12}>
-                                    <table>
-                                        <thead>
-                                            <th>
-                                                Scientific Name
-                                            </th>
-                                            <th>
-                                                Common Name
-                                            </th>
-                                            <th>
-                                                Categories
-                                            </th>
-                                            <th>
-                                                Regions
-                                            </th>
+                                <GridItem xs={12} sm={12} md={12} style={{marginTop: 30, marginLeft: '20px', marginRight: '20px'}}>
+                                    <CustomInput
+                                            labelText="Search" 
+                                    />
+                                </GridItem>
+                                <GridItem xs={12} sm={12} md={12} style={{marginTop: 20, marginLeft: '20px', marginRight: '20px'}}>
+                                    <table className={classes.table}>
+                                        <thead className={classes.tableHeader}>
+                                            <tr className={classes.tableHeaderRow}>
+                                                <th className={classes.tableHeaderColumn}>
+                                                   Scientific Name <IconArrowDropDown className={classes.tableArrowIcon}/>
+                                                </th>
+                                                <th className={classes.tableHeaderColumn}>
+                                                    Common Name <IconArrowDropDown className={classes.tableArrowIcon}/>
+                                                </th>
+                                                <th className={classes.tableHeaderColumn}>
+                                                    Categories <IconArrowDropDown className={classes.tableArrowIcon}/>
+                                                </th>
+                                            </tr>
                                         </thead>
+                                        <tbody className={classes.tableBody}>
+                                            {plantProfileRows}
+                                        </tbody>
                                     </table>
                                 </GridItem>
                             </GridContainer>
@@ -75,7 +110,19 @@ export const query = graphql`
                     ...GatsbyImageSharpFluid_withWebp
                 }
             }
-        }
+        },
+        allContentfulPlantProfile {
+            edges {
+              node {
+                scientificName,
+                slug
+                commonName,
+                categories {
+                    name
+                }
+              }
+            }
+          }
     }
 `;
 
