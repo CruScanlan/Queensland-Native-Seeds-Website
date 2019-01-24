07074dcd345b00f10594d53e07f08f0dac414da5
Cru Scanlan <cruzercru@hotmail.com>
2019-01-24 15:20:53 +1000

    Fix spelling error

--- src/sections/ContactSection.jsx
+++ src/sections/ContactSection.jsx
@@ -61,7 +61,7 @@ class ContactSection extends React.Component {
 
     encode(data) {
         return Object.keys(data)
-          .map(key => encodeURIComponent(key) + "=" + encodeURIComponen(data[key]))
+          .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
           .join("&");
     }
 
