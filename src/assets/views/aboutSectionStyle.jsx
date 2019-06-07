4e4c6388d1bf1a71006f8316368f441954b1dfc7
Cru Scanlan <cruzercru@hotmail.com>
2019-06-07 10:52:42 +1000

    Use Contentful For About Us Content

---
+++ src/assets/views/aboutSectionStyle.jsx
@@ -0,0 +1,15 @@
+const aboutSectionStyle = {
+    content: {
+        "& p": {
+            fontSize: '1.0625rem',
+            lineHeight: '1.55em',
+            marginTop: '10px',
+            marginBotton: '10px',
+            color: '#000',
+            fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+            fontWeight: 300
+        }
+    }
+}
+
+export default aboutSectionStyle;
--- src/sections/about/AboutSection1.jsx
+++ src/sections/about/AboutSection1.jsx
@@ -10,10 +10,16 @@ import { StaticQuery, graphql } from 'gatsby'
 import GridContainer from "components/Grid/GridContainer.jsx";
 import GridItem from "components/Grid/GridItem.jsx";
 import generalStyle from "assets/views/generalStyle.jsx";
+import aboutSectionStyle from "assets/views/aboutSectionStyle.jsx";
+
+const styles = {
+    ...generalStyle,
+    ...aboutSectionStyle
+}
 
 class AboutSection1 extends React.Component {
     render() {
-        const { classes } = this.props;
+        const { classes, content } = this.props;
         return (
             <StaticQuery
                 query={graphql`
@@ -34,15 +40,7 @@ class AboutSection1 extends React.Component {
                                 <GridItem xs={12} sm={12} md={8}>
                                     <h2 className={classes.title}>About Us</h2>
                                 </GridItem>
-                                <GridItem xs={12} sm={12} md={8}>
-                                    <h5 className={classes.text}>
-                                        Queensland Native Seeds is owned and managed by Lachlan Scanlan and his son Cru Scanlan. We produce native grass, tree and shrub seed for rehabilitation in mines across Queensland and NSW.
-                                    </h5>
-                                    <br />
-                                    <h5 className={classes.text}>
-                                        The supply of native plant seed has been tightly limited in the decades since native plant rehabilitation began. This is for several reasons. Native plants rely on very particular weather sequences besides their own trait tendencies to set seed in a commercially viable way and it is common for viable crops of trees and shrubs to only appear every five to ten years. There are large knowledge barriers to entry. There’s considerable risk in holding relatively high cost products reliant on a relatively small market.
-                                    </h5>
-                                </GridItem>
+                                <GridItem xs={12} sm={12} md={8} className={classes.content} dangerouslySetInnerHTML={{ __html: content }} />
                                 <GridItem xs={12} sm={12} md={4}>
                                     <div className={classes.inLineImageContainer}>
                                         <Img fluid={data.image.childImageSharp.fluid} className={classes.inLineImage} />
@@ -58,4 +56,4 @@ class AboutSection1 extends React.Component {
     }
 }
 
-export default withStyles(generalStyle)(AboutSection1);
+export default withStyles(styles)(AboutSection1);
